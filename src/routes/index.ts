f38db9ce20e1941303bb5cdc1629c6f7b4953dc5
rayane.melo <rayane.melo@lighthouseit.com.br>
2022-06-21 09:11:52 -0300

    remove: controller admin

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import events from "./eventsRouter";
-import admin from "./adminRouter";
 
 const routes = (app: express.Application) => {
   app.route("/").get((req, res) => res.status(200).send({ titulo: "Alô alô" }));
 
-  app.use(express.json(), events, admin);
+  app.use(express.json(), events);
 };
 
 export default routes;
